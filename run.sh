e9b4615e1d3e1340ad00f18077bd96dcd76f1820
Min1579 <hsm012362@gmail.com>
2020-10-07 03:23:03 +0900

    chore 📦 setup docker

---
+++ run.sh
@@ -0,0 +1,1 @@
+exec java -jar /eureka-0.0.1-SNAPSHOT.jar
--- src/main/java/com/heythere/eureka/EurekaDiscoverServerApplication.java
+++ src/main/java/com/heythere/eureka/EurekaDiscoverServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaDiscoverServerApplication {
-
 	public static void main(String... args) {
 		SpringApplication.run(EurekaDiscoverServerApplication.class, args);
 	}
-
 }
